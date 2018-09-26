8280e5ec2a68d1523bfa32777a05b3a9f69ee126
calvinpete <tinkacalvin@gmail.com>
2018-09-27 00:56:54 +0300

    [Delivers #160754751] create a register method

--- app/account.py
+++ app/account.py
@@ -5,3 +5,11 @@ class Account:
     def __init__(self):
         self.accounts = []  # A data structure to store user accounts
 
+    def register(self, user):
+        """
+        This saves an account created by a user
+        :param user:
+        :return:
+        """
+        self.accounts.append(user)
+        print("You've been successfully registered")
