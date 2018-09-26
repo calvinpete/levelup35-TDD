1fa368e5bb84d86653ef4be1f41d8fa67efa56b2
calvinpete <tinkacalvin@gmail.com>
2018-09-27 00:54:40 +0300

    create a class Account

---
+++ app/account.py
@@ -0,0 +1,7 @@
+class Account:
+    """
+    This class holds the functionality of managing a user account
+    """
+    def __init__(self):
+        self.accounts = []  # A data structure to store user accounts
+
