c5ed9d67072fe5ccbd1420febf09a212545da6b1
calvinpete <tinkacalvin@gmail.com>
2018-09-27 00:58:42 +0300

    [Delivers #160754803] create a login method

--- app/account.py
+++ app/account.py
@@ -13,3 +13,22 @@ class Account:
         """
         self.accounts.append(user)
         print("You've been successfully registered")
+
+    def login(self, username, password):
+        """
+        This allows a user to sign in to his or her account
+        :param username:
+        :param password:
+        :return:
+        """
+        for i in self.accounts:
+            if i.__getattribute__("username") != username:
+                print("Please input the right username")
+                return False
+
+            if i.__getattribute__("password") != password:
+                print("Please enter the correct password")
+                return False
+            else:
+                i.__dict__["active"] = True
+                return True
